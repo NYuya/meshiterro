8f342070ea25ac66b7da8c46fa8963cdecc0076e
NYuya <noooochan5@gmail.com>
2020-03-18 23:40:19 +0900

    [Finish5-4]

--- Gemfile
+++ Gemfile
@@ -67,6 +67,9 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 
 gem "refile-mini_magick"
 
+gem 'kaminari','~> 1.1.1'
+
+
 
 
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd4484ffd5dfdb489ab0094c635101ed06e1ff43ad903d950ce0f3b47f43bc4f70233244bcbbe1093132e6ed099480b19080c980818868478397329f040426db0'
